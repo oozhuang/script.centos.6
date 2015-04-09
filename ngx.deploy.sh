7f5cc2c9512adcccc6b4f8f3f68069c684cbe412
zhuangjiadong <jiadong.zhuang@qq.com>
2015-04-09 10:57:18 +0000

    evolve the script. with project.lualib

--- ngx.deploy.sh
+++ ngx.deploy.sh
@@ -5,25 +5,24 @@ fi
 
 openresty_path="/usr/local/openresty"
 proj_name=`basename $1`
-proj_path="$openresty_path/projects/$proj_name"
-host_path="$openresty_path/vhosts"
+proj_path="$openresty_path/project/$proj_name"
+host_path="$openresty_path/vhost"
+lualib_path="$openresty_path/project.lualib"
 
 if [[ ! -f "$1/${proj_name}.ngx.conf" ]]; then
 	echo " $1/${proj_name}.ngx.conf is missing.">&2; exit
 fi
 
-set -e
 
-sudo mkdir -p $host_path
-sudo mkdir -p $proj_path
-sudo cp $1/${proj_name}.ngx.conf $host_path/
-sudo cp -R $1/* $proj_path/
-sudo rm $proj_path/${proj_name}.ngx.conf
-
-sudo chown root:root $host_path/* -R
-sudo chown root:root $proj_path/* -R
-sudo chmod 644 $host_path/* -R
-sudo chmod 740 $proj_path/* -R
+function create_if_not_exist_dir() {
+	if [[ ! -d $1 ]]; then
+		sudo mkdir -p $1
+		sudo chown root:root $1 -R
+		if [[ "$2" != "" ]]; then
+			sudo chmod $2 $1
+		fi
+	fi
+}
 
 function func_set_mode(){
 	if [[ $# -lt 3 ]]; then echo "usage: func_set_mode {path} {dir-mode} {file-mode}">&2; return -1; fi
@@ -37,8 +36,36 @@ function func_set_mode(){
 function func_set_safe_read() { func_set_mode $1 755 644; } 
 function func_set_safe_exec() { func_set_mode $1 755 755; } 
 
+set -e
+create_if_not_exist_dir $lualib_path 755
+create_if_not_exist_dir $host_path   755
+create_if_not_exist_dir $proj_path   755
+
+sudo cp $1/${proj_name}.ngx.conf $host_path/
+sudo cp -R $1/lualib/$proj_name $lualib_path/
+sudo cp -R $1/* $proj_path/
+sudo chown root:root $lualib_path/* -R
+sudo chown root:root $host_path/* -R
+sudo chown root:root $proj_path/* -R
+sudo chmod 644 $host_path/* -R
+sudo chmod 740 $proj_path/* -R
+sudo chmod 740 $lualib_path/$proj_name -R
+
+rm_list="${proj_name}.ngx.conf .git README.md lualib"
+for f in $rm_list; do 
+	sudo rm -rf $proj_path/$f
+done
+
 func_set_safe_read $proj_path/html 
 func_set_safe_exec $proj_path/lua
+func_set_safe_read $lualib_path/$proj_name
 
 echo "$proj_path deployed."
+
+if [[ "$2" == "reload" ]]; then
+	sudo $openresty_path/nginx/sbin/nginx -t
+	if [[ $? -eq 0 ]]; then
+		sudo $openresty_path/nginx/sbin/nginx -s reload
+	fi
+fi
 
