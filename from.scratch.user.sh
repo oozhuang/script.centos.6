ab14b3dae336ecaf6801f9f5498796004bb24d1c
庄家栋 <jiadong.zhuang@qq.com>
2015-04-16 13:45:16 +0800

    bug-fixed

--- from.scratch.user.sh
+++ from.scratch.user.sh
@@ -10,16 +10,16 @@ sudo useradd -m $1
 pwd=`cat /dev/urandom | sed 's/[^a-zA-Z0-9]//g' | strings -n 6 | head -n 1`
 echo "$pwd"| sudo passwd $1 --stdin
 
+mkdir -p ~/tmp
 cat $CUR_DIR/resource/gitconfig \
-	| sudo sed "s/USERNAME/$1/g" \
-	| sudo sed "s/EMAIL/$2/g" > /home/$1/.gitconfig
-sudo chown $1:$1 /home/$1/.gitconfig
-
-sudo mkdir -p /home/.ssh
-sudo chmod 700 /home/.ssh
-touch /home/.ssh/authorized_keys
-sudo chmod 600 /home/.ssh/authorized_keys
-sudo chown $1:$1 /home/.ssh -R
+	| sed "s/USERNAME/$1/g" \
+	| sed "s/EMAIL/$2/g" > ~/tmp/gitconfig
+sudo cp ~/tmp/gitconfig /home/$1/.gitconfig
+sudo mkdir -p /home/$1/.ssh
+sudo touch /home/$1/.ssh/authorized_keys
+sudo chown $1:$1 /home/$1 -R
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
 
 echo "$1 is ready: $pwd"
 
