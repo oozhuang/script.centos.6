d9f9e494e971580ecbb35a3ef85889a3bf103874
庄家栋 <jiadong.zhuang@qq.com>
2015-05-24 21:49:02 +0800

    fix to run on darwin.

--- inc/deploy.lua.sh
+++ inc/deploy.lua.sh
@@ -53,7 +53,7 @@ function func_deploy_prepare() {
 	sudo cp    $g_src_conf    $g_tmp_conf
 
 	_list="$g_tmp_dir $g_tmp_lib_dir $g_tmp_conf"
-	for f in $_list; do tool_set_own $f root; done
+	for f in $_list; do tool_set_own $f root staff; done
 
 	tool_keep_safe $g_tmp_dir
 
@@ -63,9 +63,8 @@ function func_deploy_prepare() {
 	#_list="$g_tmp_dir/lua" # TODO keep_safe_read is ok?
 	#for f in $_list; do tool_keep_safe_exec $f; done
 	
-	sudo $g_nginx -t
+	sudo $g_nginx -t || ( func_deploy_clear && return 1 )
 
-	return $?
 }
 
 function func_deploy_finalize() {
@@ -78,23 +77,20 @@ function func_deploy_finalize() {
 	sudo mv $g_tmp_lib_dir $g_dest_lib_dir
 	sudo mv $g_tmp_conf    $g_dest_conf
 	echo "$g_name($g_ver) deployed"
-	return 0
+	return 1
 }
 
 function func_deploy_clear() {
 	_list="$g_tmp_dir $g_tmp_lib_dir $g_tmp_conf"
 	for f in $_list; do sudo rm -rf $f; done
+	echo "clear...done"
 	return 0
 }
 
 function func_deploy() {
 	func_deploy_init $1
 	func_deploy_backup
-	func_deploy_prepare
-	if [[ $? -ne 0 ]]; then
-		func_deploy_clear
-		return
-	fi
+	func_deploy_prepare || return 0
 	echo -n "src is ready. deploy? [y|n]"
 	while read a; do
 		case $a in
